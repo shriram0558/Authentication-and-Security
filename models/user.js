48d3f1a0c9bc57d1e7e8efc9abf498bd63d7ce8c
Shriram Bhaskar Sonawane <shriramsonawane143@gmail.com>
2023-02-25 16:43:15 +0530

    Level 2 - Data Encryption

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const userSchema = mongoose.Schema({
     email: String, 
     password: String
 });
+
+const secret = 'thisisoursecretkey.';
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 module.exports = mongoose.model('User', userSchema);
